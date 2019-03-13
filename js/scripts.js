3af6fef6271738f75f9cf2ea648ee88d3f0e5fb7
Paul Beffa <paulbeffa@outlook.fr>
2019-03-13 20:37:38 +0100

    Searchbar created

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,8 @@
 // METDIGGER APP by Paul Beffa
 // Search in the Metropolitan Museum art collection
-
-
-// Get our root div to start manipulating the dom inside it
+const searchbar = document.getElementById('searchbar');
 const app = document.getElementById('root');
 
-// Create the logo
-const logo = document.createElement('img');
-logo.src = 'image/logo.jpg';
 
 // Create a container
 const container = document.createElement('div');
@@ -15,124 +10,131 @@ container.setAttribute('class', 'container');
 
 
 // Add the logo and the main container to index.html
-app.appendChild(logo);
 app.appendChild(container);
 
+searchbar.addEventListener('keypress', function(e){
 
-function addCard(imgsrc, title, description, link) {
-    
-    
-    // ES6 way of addign card
+    if (e.keyCode == 13) {
 
 
-    let template = `<hr class="featurette-divider">
+        function addCard(imgsrc, title, description, link) {
+            
+            // ES6 way of addign card
 
-    <div class="row featurette">
-    <div class="col-md-7">
-      <h2 class="featurette-heading">${title}</h2>
-      <p class="lead">${description}</p>
-      <a class="metlink" href=${link}>Read on The Met</a>
-    </div>
-    <div class="col-md-5">
-      <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto img-limit" src=${imgsrc} preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Photo"></img>
-    </div>
-  </div>`
+            let template = `<hr class="featurette-divider">
 
-  container.innerHTML += template;
+            <div class="row featurette">
+            <div class="col-md-7">
+            <h2 class="featurette-heading">${title}</h2>
+            <p class="lead">${description}</p>
+            <a class="metlink" href=${link}>Read on The Met</a>
+            </div>
+            <div class="col-md-5">
+            <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto img-limit" src=${imgsrc} preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Photo"></img>
+            </div>
+        </div>`
 
-
-    
-    /* ES5 way of adding Card below
-
-    // create card elements
-    cardContainer = document.createElement('container')
-    img = document.createElement('img');
-    h1 = document.createElement('h1');
-    p = document.createElement('p');
-    button = document.createElement('a');
-
-    // style card elements
+        container.innerHTML += template;
 
 
+            
+            /* ES5 way of adding Card below
 
-    // insert function parameter in element
-    img.setAttribute('src', imgsrc);
-    h1.innerText = title;
-    p.innerText = description;
-    button.setAttribute('href', link);
+            // create card elements
+            cardContainer = document.createElement('container')
+            img = document.createElement('img');
+            h1 = document.createElement('h1');
+            p = document.createElement('p');
+            button = document.createElement('a');
 
-    //add elements to the DOM
-
-    container.appendChild(cardContainer);
-    cardContainer.appendChild(img);
-    cardContainer.appendChild(h1);
-    cardContainer.appendChild(p);
-    cardContainer.appendChild(button); */
-
-};
+            // style card elements
 
 
 
+            // insert function parameter in element
+            img.setAttribute('src', imgsrc);
+            h1.innerText = title;
+            p.innerText = description;
+            button.setAttribute('href', link);
 
+            //add elements to the DOM
+
+            container.appendChild(cardContainer);
+            cardContainer.appendChild(img);
+            cardContainer.appendChild(h1);
+            cardContainer.appendChild(p);
+            cardContainer.appendChild(button); */
+
+        };
 
 
 
 
-// Create a request variable and assign a new XMLHttpRequest object to it.
-const request = new XMLHttpRequest();
 
-// Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://collectionapi.metmuseum.org/public/collection/v1/search?q=sunflowers', true);
 
-request.onload = function () {
 
-   // Begin accessing JSON data here
-   let data = JSON.parse(this.response);
-   
-    
-   if (request.status >= 200 && request.status < 400) {
 
-    data.objectIDs.forEach(element => {
-       let index = element.toString();
+        // Create a request variable and assign a new XMLHttpRequest object to it.
+        const request = new XMLHttpRequest();
+
+        // Open a new connection, using the GET request on the URL endpoint
+        request.open('GET', 'https://collectionapi.metmuseum.org/public/collection/v1/search?q=' + searchbar.value, true);
+
+        request.onload = function () {
+
+        // Begin accessing JSON data here
+        let data = JSON.parse(this.response);
         
-   
-        // OBJECT REQUEST : getting the artwork for a specific ID
-
-        const objectrequest = new XMLHttpRequest();
-
-            // Open a new connection, using the GET request on the URL endpoint
-            objectrequest.open('GET', 'https://collectionapi.metmuseum.org/public/collection/v1/objects/' + index, true);
-
-            objectrequest.onload = function () {
-
-            // Begin accessing JSON data here
-            let object = JSON.parse(this.response);
-                
-            if (objectrequest.status >= 200 && objectrequest.status < 400) {
             
-                addCard(object.primaryImageSmall, object.title, object.artistDisplayName, object.objectURL);
-    
+        if (request.status >= 200 && request.status < 400) {
 
-                } else {
+            data.objectIDs.forEach(element => {
+            let index = element.toString();
                 
-                    const errorMessage = document.createElement('p');
-                    errorMessage.textContent = `Couldn't fetch data`;
-                    app.appendChild(errorMessage);
-                }
+        
+                // OBJECT REQUEST : getting the artwork for a specific ID
+
+                const objectrequest = new XMLHttpRequest();
+
+                    // Open a new connection, using the GET request on the URL endpoint
+                    objectrequest.open('GET', 'https://collectionapi.metmuseum.org/public/collection/v1/objects/' + index, true);
+
+                    objectrequest.onload = function () {
+
+                    // Begin accessing JSON data here
+                    let object = JSON.parse(this.response);
+                        
+                    if (objectrequest.status >= 200 && objectrequest.status < 400) {
+                    
+                        addCard(object.primaryImageSmall, object.title, object.artistDisplayName, object.objectURL);
             
-            };
 
-        objectrequest.send();
+                        } else {
+                        
+                            const errorMessage = document.createElement('p');
+                            errorMessage.textContent = `Couldn't fetch data`;
+                            app.appendChild(errorMessage);
+                        }
+                    
+                    };
 
-    });
+                objectrequest.send();
+
+            });
 
 
-    } else {
-    
-        const errorMessage = document.createElement('p');
-        errorMessage.textContent = `Couldn't fetch data`;
-        app.appendChild(errorMessage);
+            } else {
+            
+                const errorMessage = document.createElement('p');
+                errorMessage.textContent = `Couldn't fetch data`;
+                app.appendChild(errorMessage);
+            }
+        };
+
+        request.send();
+
     }
-};
 
-request.send();
+}); // END MAIN
+
+
